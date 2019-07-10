fa883718a20b277d267e8493d8bdd147c8305196
platoneko <platonekosama@gmail.com>
2019-07-11 00:25:27 +0800

    update criterions

--- modules/criterions.py
+++ modules/criterions.py
@@ -4,51 +4,71 @@ from torch.nn.modules.loss import _Loss
 
 class SequenceNLLLoss(_Loss):
     """
-    NLLLoss for sequence, usually ignores padding index
+    NLLLoss for sequence, average/sum the loss across the batches
     """
-    def __init__(self, weight=None, ignore_index=-100, reduction='mean'):
+    def __init__(self, weight=None, padding_idx=None, reduction='mean'):
         super().__init__()
         assert reduction in ['none', 'sum', 'mean']
         self.register_buffer('weight', weight)
-        self.ignore_index = ignore_index
+        self.padding_idx = padding_idx
         self.reduction = reduction
 
-    def forward(self, inputs, target):
+    def forward(self, input, target, reduction=True):
         """
-        inputs: (batch_size, max_len, num_classes)
+        input: (batch_size, max_len, vocab_size)
         target: (batch_size, max_len)
         """
+        if self.weight is None and self.padding_idx is not None:
+            self.weight = input.new_ones(input.size(-1))
+        batch_size = input.size(0)
         nll = F.nll_loss(
-            input=inputs.reshape(-1, inputs.size(-1)),
+            input=input.reshape(-1, input.size(-1)),
             target=target.reshape(-1),
             weight=self.weight,
-            ignore_index=self.ignore_index,
-            reduction=self.reduction
+            reduction='none'
         )
+        nll = nll.view(batch_size, -1).sum(dim=1)
+
+        if reduction:
+            if self.reduction == 'mean':
+                nll = nll.mean()
+            elif self.reduction == 'sum':
+                nll = nll.sum()
+
         return nll
 
 
 class SequenceCrossEntropy(_Loss):
     """
-    Cross entropy for sequence, usually ignores padding index
+    Cross entropy for sequence, average/sum the loss across the batches
     """
-    def __init__(self, weight=None, ignore_index=-100, reduction='mean'):
+    def __init__(self, weight=None, padding_idx=None, reduction='mean'):
         super().__init__()
         assert reduction in ['none', 'sum', 'mean']
         self.register_buffer('weight', weight)
-        self.ignore_index = ignore_index
+        self.padding_idx = padding_idx
         self.reduction = reduction
 
-    def forward(self, inputs, target):
+    def forward(self, input, target, reduction=True):
         """
-        inputs: (batch_size, max_len, num_classes)
+        input: (batch_size, max_len, vocab_size)
         target: (batch_size, max_len)
         """
+        if self.weight is None and self.padding_idx is not None:
+            self.weight = input.new_ones(input.size(-1))
+        batch_size = input.size(0)
         cross_entropy = F.cross_entropy(
-            input=inputs.reshape(-1, inputs.size(-1)),
+            input=input.reshape(-1, input.size(-1)),
             target=target.reshape(-1),
             weight=self.weight,
-            ignore_index=self.ignore_index,
-            reduction=self.reduction
+            reduction='none'
         )
+        cross_entropy = cross_entropy.view(batch_size, -1).sum(dim=1)
+
+        if reduction:
+            if self.reduction == 'mean':
+                cross_entropy = cross_entropy.mean()
+            elif self.reduction == 'sum':
+                cross_entropy = cross_entropy.sum()
+
         return cross_entropy
